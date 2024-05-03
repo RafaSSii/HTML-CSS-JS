e4b8c90788ebd1c6bcd8ffba3beb0b454990f0b5
Rafael Santos <149257772+RafaSSii@users.noreply.github.com>
2024-05-02 22:25:14 -0300

    Add files via upload

---
+++ COLOR_SELECTOR/script.js
@@ -0,0 +1,31 @@
+function color(valor){
+var color_show = document.getElementById("color-show");
+
+
+if(valor == "red"){
+    color_show.style.backgroundColor = 'red';
+    color_show.innerText = 'RED'
+}
+if(valor == "yellow"){
+    color_show.style.backgroundColor = 'yellow';
+    color_show.innerText = 'YELLOW'
+    
+}
+ if(valor == "blue"){
+    color_show.style.backgroundColor = 'blue';
+    color_show.innerText = 'BLUE'
+}
+ if(valor == "green"){
+    color_show.style.backgroundColor = 'green';
+    color_show.innerText = 'GREEN'
+}
+
+
+
+}
+
+    
+    
+
+
+
